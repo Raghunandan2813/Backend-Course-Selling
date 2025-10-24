bc2751ad5ee977418ba76ee1edb2a44af4a380bd
Raghunandan <raghu9555k@gmail.com>
2025-10-24 19:29:30 +0530

    creating database

---
+++ db.js
@@ -0,0 +1,30 @@
+import { Schema } from "mongoose";
+
+
+const userSchema= Schema({
+
+})
+
+
+const adminSchema= Schema({
+
+
+})
+
+
+const courseSchema= Schema({
+
+
+})
+
+
+const purchaseSchema= Schema({
+
+
+})
+
+
+const userModel = mongoose.Model("user", userSchema)
+const adminModel = mongoose.Model("admin", userSchema)
+const courseModel = mongoose.Model("course", userModel)
+const purchaseModel = mongoose.Model("purchase", userModel)
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app= express();
 
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/course', courseRouter)
+app.use("/api/v1/admin", adminRouter)
 
 app.listen(3000);
 
